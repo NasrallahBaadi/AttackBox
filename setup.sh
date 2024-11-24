83e1966d1d35b7b623300693df68214e99bea36a
Nasrallah <33826467+NasrallahBaadi@users.noreply.github.com>
2024-11-24 16:51:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ sudo mv vpnbash.sh vpnserver.sh /opt && mv tmux.png ~/Pictures
 cp ~/.bashrc .bashrc.bak && mv bashrc .bashrc
 cat aliases >> .bashrc
 mkdir -p ~/ctf/htb ~/ctf/thm
-mv www ~/ctf
 dconf load /org/mate/terminal/ < hackthebox.profile
 cd ~/ctf && git clone https://github.com/NasrallahBaadi/www
 sudo apt install xclip
+cp tmux.desktop ~/.local/share/applications/
+ssh-keygen -f ~/ctf/www/id_rsa
+cp zshrc ~/.zshrc
